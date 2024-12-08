d4494224ef9a33950798db0cd1586e0f7b7f8ab8
Carlos Galindo <utm22030587@utma.edu.mx>
2024-12-08 17:34:29 -0600

    refactor: create middleware to get jwt from cookie

---
+++ library-utma-backend/Helpers/AuthMiddleware.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Text;
+
+namespace library_utma_backend.Helpers
+{
+    public class AuthMiddleware : ActionFilterAttribute
+    {
+        private readonly string _issuer;
+        private readonly string _audience;
+        private readonly string _key;
+
+        public AuthMiddleware()
+        {
+            // Leer configuraciones desde variables de entorno
+            _issuer = Environment.GetEnvironmentVariable("ISSUER") ?? throw new ArgumentNullException("ISSUER no configurado");
+            _audience = Environment.GetEnvironmentVariable("AUDIENCE") ?? throw new ArgumentNullException("AUDIENCE no configurado");
+            _key = Environment.GetEnvironmentVariable("KEY") ?? throw new ArgumentNullException("KEY no configurado");
+        }
+
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            // Obtener el valor de la cookie "AuthToken"
+            var cookie = context.HttpContext.Request.Cookies["AuthToken"];
+
+            Console.WriteLine($"Cookie: {cookie}");
+
+            if (string.IsNullOrWhiteSpace(cookie))
+            {
+                context.Result = new UnauthorizedObjectResult("Token no proporcionado.");
+                return;
+            }
+
+            try
+            {
+                // Validar el token
+                var handler = new JwtSecurityTokenHandler();
+                var tokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = _issuer,
+                    ValidAudience = _audience,
+                    IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_key))
+                };
+
+                handler.ValidateToken(cookie, tokenValidationParameters, out var validatedToken);
+
+                // Extraer el token JWT
+                var jwtToken = validatedToken as JwtSecurityToken;
+
+                if (jwtToken == null)
+                {
+                    context.Result = new UnauthorizedObjectResult("Token inválido.");
+                    return;
+                }
+
+                // Validar los claims personalizados
+                var userIdClaim = jwtToken.Claims.FirstOrDefault(claim => claim.Type == "Id")?.Value;
+                var userTypeClaim = jwtToken.Claims.FirstOrDefault(claim => claim.Type == "Utstr")?.Value;
+
+                if (string.IsNullOrWhiteSpace(userIdClaim) || string.IsNullOrWhiteSpace(userTypeClaim))
+                {
+                    context.Result = new UnauthorizedObjectResult("Claims faltantes en el token.");
+                    return;
+                }
+
+                if (!int.TryParse(userIdClaim, out int userId) || userId <= 0)
+                {
+                    context.Result = new UnauthorizedObjectResult("ID de usuario inválido.");
+                    return;
+                }
+
+                // Almacenar información en el contexto
+                context.HttpContext.Items["UserRole"] = userTypeClaim;
+                context.HttpContext.Items["UserId"] = userId;
+            }
+            catch (SecurityTokenException ex)
+            {
+                context.Result = new UnauthorizedObjectResult($"Token inválido: {ex.Message}");
+                return;
+            }
+
+            base.OnActionExecuting(context);
+        }
+    }
+}
--- library-utma-backend/Program.cs
+++ library-utma-backend/Program.cs
@@ -1,17 +1,11 @@
 using library_utma_backend.Context;
 using library_utma_backend.Helpers;
 using library_utma_backend.Models;
-using library_utma_backend.SwaggerConfig;
-using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
-using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
-using System.Reflection;
-using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 
@@ -20,6 +14,7 @@ var connection = builder.Configuration.GetConnectionString("Connection");
 builder.Services.AddDbContextPool<AppDbContext>(options =>
     options.UseMySql(connection, new MySqlServerVersion(new Version(8, 0, 39))
 ));
+
 builder.Services.AddSingleton<JsonWebToken>();
 builder.Services.AddSingleton<User>();
 
@@ -37,50 +32,6 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
 {
     c.SwaggerDoc("v1", new OpenApiInfo { Title = "Library-UTMA-API", Version = "v1" });
-
-    // Definir el esquema de seguridad
-    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "Bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "Introduce el JWT in format: Bearer {token}"
-    });
-
-    //var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
-    //var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
-    //c.IncludeXmlComments(xmlPath);
-
-    c.OperationFilter<AuthorizeCheckOperationFilter>();
-});
-
-builder.Services.AddAuthentication(options =>
-{
-    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
-})
-.AddJwtBearer(options =>
-{
-    options.TokenValidationParameters = new TokenValidationParameters
-    {
-        ValidateIssuer = true,
-        ValidateAudience = true,
-        ValidateLifetime = true,
-        ValidateIssuerSigningKey = true,
-        ValidIssuer = System.Environment.GetEnvironmentVariable("ISSUER"),
-        ValidAudience = System.Environment.GetEnvironmentVariable("AUDIENCE"),
-        IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(System.Environment.GetEnvironmentVariable("KEY")))
-    };
-    options.Events = new JwtBearerEvents
-    {
-        OnMessageReceived = context =>
-        {
-            context.Token = context.Request.Cookies["AuthToken"];
-            return Task.CompletedTask;
-        }
-    };
 });
 
 var app = builder.Build();
@@ -95,8 +46,6 @@ if (app.Environment.IsDevelopment())
 app.UseHttpsRedirection();
 
 app.UseCors("AllowSpecificOrigins");
-
-app.UseAuthentication();
 
 app.UseAuthorization();
 
