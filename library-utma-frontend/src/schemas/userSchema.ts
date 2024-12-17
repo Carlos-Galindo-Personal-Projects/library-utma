1eacbc27981a3e7b28aa063af429f2d266a6ce34
Carlos Galindo <utm22030587@utma.edu.mx>
2024-12-17 14:05:25 -0600

    reaft: add message if email has more than 32 characters

--- library-utma-frontend/src/schemas/userSchema.ts
+++ library-utma-frontend/src/schemas/userSchema.ts
@@ -4,7 +4,7 @@ export const userSchema = z.object({
   email: z
     .string()
     .email({ message: 'Correo electrónico inválido' })
-    .max(32),
+    .max(32, { message: 'El correo electrónico debe tener como máximo 32 caracteres' }),
   password: z
     .string()
     .min(6, { message: 'La contraseña debe tener al menos 6 caracteres' }),
