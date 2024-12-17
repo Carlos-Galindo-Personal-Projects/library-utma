0d88d48d0d3a007e53d1f14c12c84c8ea2ae6b10
Carlos Galindo <utm22030587@utma.edu.mx>
2024-12-17 16:07:09 -0600

    feat: show register form

--- library-utma-frontend/src/app/auth/home/users/page.tsx
+++ library-utma-frontend/src/app/auth/home/users/page.tsx
@@ -2,12 +2,6 @@ import RegisterForm from "./_components/registerForm";
 
 export default function UsersPage() {
   return (
-    <div
-      className="flex flex-col items-center justify-center h-full"
-    >
-      <div>
-        <RegisterForm />
-      </div>
-    </div>
+    <RegisterForm />
   );
 }
