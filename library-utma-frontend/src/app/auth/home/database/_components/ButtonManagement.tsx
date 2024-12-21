da53829cca13b6ce903504b9c6baa78abc17dd2b
Carlos Galindo <utm22030587@utma.edu.mx>
2024-12-21 16:00:21 -0600

    style: add transition to button when is clicked

--- library-utma-frontend/src/app/auth/home/database/_components/ButtonManagement.tsx
+++ library-utma-frontend/src/app/auth/home/database/_components/ButtonManagement.tsx
@@ -10,7 +10,7 @@ const ButtonManagement = () => {
 
     return (
         <button
-            className="bg-red-600 hover:bg-red-700 text-lg px-16 py-1 rounded-xl shadow-md"
+            className="text-white bg-red-600 hover:bg-red-700 active:bg-red-800 focus:ring-2 focus:ring-red-300 focus:outline-none p-1 transition text-lg px-16 py-1 rounded-xl shadow-md"
             onClick={handleClick}
         >
             Eliminar Datos
