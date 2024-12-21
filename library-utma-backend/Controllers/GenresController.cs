9bde3f6b42de5bc114f8aec237ab5ac0cfe13379
Carlos Galindo <utm22030587@utma.edu.mx>
2024-12-21 17:27:15 -0600

    feat: create endpoint for getting genres

---
+++ library-utma-backend/Controllers/GenresController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using library_utma_backend.Context;
+using library_utma_backend.Models;
+using library_utma_backend.DTO;
+
+namespace library_utma_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GenresController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public GenresController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Genres
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<GenresResponseDTO>>> GetGenres()
+        {
+            try
+            {
+                var genres = await _context.Genre.
+                    Select(g => new GenresResponseDTO
+                    {
+                        Id = g.Id,
+                        Name = g.Name
+                    })
+                    .ToListAsync();
+
+                if (genres.Count == 0)
+                {
+                    return NoContent();
+                }
+
+                return Ok(genres);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Error interno del servidor: {e.Message}");
+            }
+        }
+    }
+}
---
+++ library-utma-backend/DTO/GenresResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace library_utma_backend.DTO
+{
+    public class GenresResponseDTO
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
