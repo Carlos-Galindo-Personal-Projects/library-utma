41a20d3491aa11eebe21ec3fa8ae637d9d574d7a
Carlos Galindo <utm22030587@utma.edu.mx>
2024-12-21 17:57:13 -0600

    set limit time with 3 seconds

--- library-utma-frontend/src/axios/axios.ts
+++ library-utma-frontend/src/axios/axios.ts
@@ -2,7 +2,7 @@ import axios from "axios"
 
 const axiosInstance = axios.create({
     baseURL: process.env.NEXT_PUBLIC_API_URL,
-    timeout: 1000,
+    timeout: 3000,
     withCredentials: true
 })
 
