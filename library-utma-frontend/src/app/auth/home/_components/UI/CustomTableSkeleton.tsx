991f8d85e53674b8cd409d19013b1be886233273
Carlos Galindo <utm22030587@utma.edu.mx>
2024-12-22 17:17:21 -0600

    style: add selector and nav buttons to skeleton

--- library-utma-frontend/src/app/auth/home/_components/UI/CustomTableSkeleton.tsx
+++ library-utma-frontend/src/app/auth/home/_components/UI/CustomTableSkeleton.tsx
@@ -3,33 +3,43 @@ import { FC } from "react";
 
 const SkeletonTable: FC<SkeletonTableProps> = ({ columns }) => {
     return (
-        <div className="shadow-md rounded-lg mt-4 overflow-x-hidden">
-            <table className="min-w-full table-auto divide-y divide-[#0F907C]">
-                <thead>
-                    <tr>
-                        {Array.from({ length: columns }).map((_, index) => (
-                            <th
-                                key={index}
-                                className="px-4 py-3 bg-[#0F907C] text-left text-xs font-medium text-gray-400 uppercase tracking-wider"
-                            >
-                                <div className="h-4 bg-gray-200 rounded w-24 animate-pulse"></div>
-                            </th>
-                        ))}
-                    </tr>
-                </thead>
-                <tbody className="bg-[#072E33] divide-y divide-[#072E33]">
-                    {Array.from({ length: 7 }).map((_, rowIndex) => (
-                        <tr key={rowIndex}>
-                            {Array.from({ length: columns }).map((_, colIndex) => (
-                                <td key={colIndex} className="px-4 py-3 whitespace-nowrap">
-                                    <div className="h-4 bg-gray-500 rounded animate-pulse"></div>
-                                </td>
+        <>
+            <div className="flex justify-center items-center animate-pulse">
+                <div className="h-10 bg-gray-200 rounded-lg px-28 mb-6"></div>
+            </div>
+
+            <div className="flex justify-center items-center space-x-8 animate-pulse">
+                <div className="h-10 w-10 bg-gray-200 transition rounded-lg px-6"></div>
+                <div className="h-10 w-10 bg-gray-200 transition rounded-lg px-6"></div>
+            </div>
+            <div className="shadow-md rounded-lg mt-4 overflow-x-hidden">
+                <table className="min-w-full table-auto divide-y divide-[#0F907C]">
+                    <thead>
+                        <tr>
+                            {Array.from({ length: columns }).map((_, index) => (
+                                <th
+                                    key={index}
+                                    className="px-4 py-3 bg-[#0F907C] text-left text-xs font-medium text-gray-400 uppercase tracking-wider"
+                                >
+                                    <div className="h-4 bg-gray-200 rounded w-24 animate-pulse"></div>
+                                </th>
                             ))}
                         </tr>
-                    ))}
-                </tbody>
-            </table>
-        </div>
+                    </thead>
+                    <tbody className="bg-[#072E33] divide-y divide-[#072E33]">
+                        {Array.from({ length: 7 }).map((_, rowIndex) => (
+                            <tr key={rowIndex}>
+                                {Array.from({ length: columns }).map((_, colIndex) => (
+                                    <td key={colIndex} className="px-4 py-3 whitespace-nowrap">
+                                        <div className="h-4 bg-gray-500 rounded animate-pulse"></div>
+                                    </td>
+                                ))}
+                            </tr>
+                        ))}
+                    </tbody>
+                </table>
+            </div>
+        </>
     );
 };
 
