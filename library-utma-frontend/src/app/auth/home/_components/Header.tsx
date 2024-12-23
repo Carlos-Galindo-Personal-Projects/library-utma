4ec78983c24f7fe726651f2d3b9402e4b67e97ad
Carlos Galindo <utm22030587@utma.edu.mx>
2024-12-23 00:08:24 -0600

    feat: show user name on header

--- library-utma-frontend/src/app/auth/home/_components/Header.tsx
+++ library-utma-frontend/src/app/auth/home/_components/Header.tsx
@@ -1,16 +1,13 @@
-"use client"
-
 import Image from "next/image";
 import Link from "next/link";
-import UserIcon from "./Icons/UserIcon";
-import { useState } from "react";
-import LogoutButton from "./LogoutButton";
+import { headers } from "next/headers";
+import ToggleDown from "./ToggleDown";
 
-export default function Header() {
+export default async function Header() {
 
-    const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+    const head = await headers();
 
-    const toggleDropdown = () => setIsDropdownOpen(prev => !prev);
+    const nameHeaders = head.get('name');
 
     return (
         <div className="flex justify-between items-center bg-[#0F907C] text-white px-8 py-1">
@@ -31,22 +28,9 @@ export default function Header() {
 
             <div className="flex items-center space-x-4">
                 <p className="text-sm">
-                    Hola, <span className="font-semibold">Admin</span>
+                    Hola, <span className="font-semibold">{nameHeaders}</span>
                 </p>
-                <div className="relative">
-                    <button
-                        onClick={toggleDropdown}
-                        className={`flex items-center justify-center w-10 h-10 rounded-full ${isDropdownOpen ? "bg-[#072E33]" : "hover:bg-[#072E33]"}`}
-                    >
-                        <UserIcon isDropdownOpen={isDropdownOpen} />
-                    </button>
-
-                    {isDropdownOpen && (
-                        <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg overflow-hidden z-10">
-                            <LogoutButton />
-                        </div>
-                    )}
-                </div>
+                <ToggleDown />
             </div>
         </div>
     );
---
+++ library-utma-frontend/src/app/auth/home/_components/ToggleDown.tsx
@@ -0,0 +1,31 @@
+"use client"
+
+import React, { useState } from 'react'
+import UserIcon from './Icons/UserIcon';
+import LogoutButton from './LogoutButton';
+
+const ToggleDown = () => {
+
+    const [isDropdownOpen, setIsDropdownOpen] = useState<boolean>(false);
+
+    const toggleDropdown = () => setIsDropdownOpen(prev => !prev);
+
+    return (
+        <div className="relative">
+            <button
+                onClick={toggleDropdown}
+                className={`flex items-center justify-center w-10 h-10 rounded-full ${isDropdownOpen ? "bg-[#072E33]" : "hover:bg-[#072E33]"}`}
+            >
+                <UserIcon isDropdownOpen={isDropdownOpen} />
+            </button>
+
+            {isDropdownOpen && (
+                <div className="absolute right-0 mt-2 w-48 bg-white shadow-lg rounded-lg overflow-hidden z-10">
+                    <LogoutButton />
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default ToggleDown
