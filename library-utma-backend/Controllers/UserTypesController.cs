0930d0104f814a776414c3b73c19701904bfb652
Carlos Galindo <utm22030587@utma.edu.mx>
2024-12-28 12:33:31 -0600

    feat: create endpoint to get user types

---
+++ library-utma-backend/Controllers/UserTypesController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using library_utma_backend.Context;
+using library_utma_backend.Models;
+using library_utma_backend.DTO.UserTypes;
+
+namespace library_utma_backend.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserTypesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public UserTypesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/UserTypes
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserTypeResponseDTO>>> GetUserType()
+        {
+            try
+            {
+                var userTypes = await _context.UserType
+                    .Select(ut => new UserTypeResponseDTO
+                    {
+                        Id = ut.Id,
+                        Name = ut.Name
+                    })
+                    .ToListAsync();
+
+                if (userTypes.Count == 0)
+                {
+                    return NoContent();
+                }
+
+                return Ok(userTypes);
+            }
+            catch (Exception e)
+            {
+                return StatusCode(500, $"Error interno del servidor: {e.Message}");
+            }
+        }
+    }
+}
---
+++ library-utma-backend/DTO/UserTypes/UserTypeResponseDTO.cs
@@ -0,0 +1,8 @@
+﻿namespace library_utma_backend.DTO.UserTypes
+{
+    public class UserTypeResponseDTO
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+    }
+}
