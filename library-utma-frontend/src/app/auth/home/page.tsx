cc006eee3c2cf036e7864bb6d963351d5923dc2e
Carlos Galindo <utm22030587@utma.edu.mx>
2024-12-28 12:15:14 -0600

    feat: add landing page

--- library-utma-frontend/src/app/auth/home/page.tsx
+++ library-utma-frontend/src/app/auth/home/page.tsx
@@ -1,10 +1,25 @@
 export default function HomePage() {
     return (
-        <div>
-            <h1 className="text-4xl font-bold">Main</h1>
-            <p className="text-lg text-gray-500">
-                This is the main page
-            </p>
+        <div className="bg-[#05161A] flex flex-col items-center justify-center h-full">
+            <div className="max-w-4xl text-center">
+                <h1 className="text-5xl font-extrabold text-[#6DA5C0] mb-6">
+                    Bienvenido al Sistema de Biblioteca UTMA
+                </h1>
+                <p className="text-lg text-[#0F907C] mb-8">
+                    Gestiona tus libros, usuarios y registros fácilmente con nuestro sistema moderno y eficiente.
+                </p>
+                <div className="space-y-4">
+                    <div className="px-6 py-3 bg-[#0F907C] text-[#05161A] rounded-lg text-lg font-medium shadow-md">
+                        Catálogo: Explora y administra los libros disponibles.
+                    </div>
+                    <div className="px-6 py-3 bg-[#6DA5C0] text-[#05161A] rounded-lg text-lg font-medium shadow-md">
+                        Usuarios: Administra los usuarios de la biblioteca.
+                    </div>
+                    <div className="px-6 py-3 bg-[#05161A] border-2 border-[#6DA5C0] text-[#6DA5C0] rounded-lg text-lg font-medium shadow-md">
+                        Actividad: Revisa los registros de préstamos y devoluciones.
+                    </div>
+                </div>
+            </div>
         </div>
-    )
+    );
 }
